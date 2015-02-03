700af471c0a2de351d9b2ec606ecf92d2c5d2a01
anagaf <anagaf@gmail.com>
2015-02-03 13:31:54 +0300

    Added BaseEdit and RangeChannelEdit

---
+++ FreqHelper/src/main/java/com/anagaf/freqhelper/BaseEdit.java
@@ -0,0 +1,80 @@
+package com.anagaf.freqhelper;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public abstract class BaseEdit extends EditText {
+    private String mBackupText;
+    private Listener mListener;
+
+    public BaseEdit(Context context) {
+        super(context);
+        init();
+    }
+
+    public BaseEdit(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public BaseEdit(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public abstract void setValue(int value);
+
+    private void init() {
+        setOnFocusChangeListener(new OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    mBackupText = getText().toString();
+                    setText("");
+                } else if (mBackupText != null) {
+                    setText(mBackupText);
+                }
+            }
+        });
+        setOnEditorActionListener(new OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    final String text = getText().toString();
+                    setValue(text.isEmpty() ? 0 : Integer.valueOf(text));
+
+                    mBackupText = null;
+
+                    InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(getWindowToken(), 0);
+
+                    mListener.onValueChanged();
+
+                    clearFocus();
+
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    public void setListener(Listener listener) {
+        mListener = listener;
+    }
+
+    /**
+     * ******* Inner Classes *********
+     */
+
+    public interface Listener {
+        public void onValueChanged();
+    }
+}
