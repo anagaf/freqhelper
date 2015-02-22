6ab7537b0d134b19ffb6a9f146b8982347849480
anagaf <Ebnuza5093>
2015-02-22 15:00:33 +0300

    Fixed BackStack test

--- FreqHelper/src/androidTest/java/com/anagaf/freqhelper/BackStackTest.java
+++ FreqHelper/src/androidTest/java/com/anagaf/freqhelper/BackStackTest.java
@@ -5,14 +5,13 @@ import android.test.AndroidTestCase;
 public class BackStackTest extends AndroidTestCase {
     public void test() {
         final BackStack backStack = BackStack.getsInstance();
-        final Frequency frequency1 = Frequency.newChannelFrequency(475, 133, 0);
-        final Frequency frequency2 = Frequency.newChannelFrequency(446, 6, 250);
+        final BackStack.Item item1 = new BackStack.Item(3, Frequency.newChannelFrequency(475, 133, 0));
+        final BackStack.Item item2 = new BackStack.Item(5, Frequency.newCtcssFrequency(34, 5));
         assertNull(backStack.pop());
-        backStack.push(frequency1);
-        backStack.push(frequency2);
-        backStack.push(frequency2);
-        assertEquals(backStack.pop(), frequency2);
-        assertEquals(backStack.pop(), frequency1);
+        backStack.push(item1);
+        backStack.push(item2);
+        assertEquals(backStack.pop(), item2);
+        assertEquals(backStack.pop(), item1);
         assertNull(backStack.pop());
     }
 }
