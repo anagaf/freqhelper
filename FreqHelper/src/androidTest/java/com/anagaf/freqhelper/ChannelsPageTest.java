9009d78d61e66dc8992d827e684f1ab55f607691
anagaf <Ebnuza5093>
2015-02-28 14:49:13 +0300

    Fixed ChannelPageTest

--- FreqHelper/src/androidTest/java/com/anagaf/freqhelper/ChannelsPageTest.java
+++ FreqHelper/src/androidTest/java/com/anagaf/freqhelper/ChannelsPageTest.java
@@ -1,20 +1,27 @@
 package com.anagaf.freqhelper;
 
 import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
 import android.test.ActivityTestCase;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
+import com.anagaf.freqhelper.ui.MainActivity;
 import com.anagaf.freqhelper.ui.views.RangeView;
 import com.anagaf.freqhelper.ui.views.ValueComponentEdit;
 
-public class ChannelsPageTest extends ActivityTestCase {
+public class ChannelsPageTest extends ActivityInstrumentationTestCase2<MainActivity> {
     private ValueComponentEdit mMhzEdit;
     private ValueComponentEdit mKhzEdit;
     private ValueComponentEdit mHzEdit;
 
     private RangeView mLpd69View;
+
+
+    public ChannelsPageTest() {
+        super(MainActivity.class);
+    }
 
     @Override
     public void setUp() throws Exception {
@@ -22,20 +29,25 @@ public class ChannelsPageTest extends ActivityTestCase {
 
         Activity activity = getActivity();
 
-        mMhzEdit = (ValueComponentEdit) getActivity().findViewById(R.id.frequency_mhz_edit);
-        mKhzEdit = (ValueComponentEdit) getActivity().findViewById(R.id.frequency_khz_edit);
-        mHzEdit = (ValueComponentEdit) getActivity().findViewById(R.id.frequency_hz_edit);
+        mMhzEdit = (ValueComponentEdit) activity.findViewById(R.id.frequency_mhz_edit);
+        mKhzEdit = (ValueComponentEdit) activity.findViewById(R.id.frequency_khz_edit);
+        mHzEdit = (ValueComponentEdit) activity.findViewById(R.id.frequency_hz_edit);
 
         ViewGroup rangesLayout = (ViewGroup) getActivity().findViewById(R.id.ranges_layout);
         mLpd69View = (RangeView) rangesLayout.getChildAt(0);
     }
 
-    public void testSetFrequency() throws Exception {
-        mMhzEdit.setValueComponent(433);
-        mKhzEdit.setValueComponent(75);
-        mHzEdit.setValueComponent(0);
+    public void testSetFrequency() throws Throwable {
+        runTestOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mMhzEdit.setValueComponent(433);
+                mKhzEdit.setValueComponent(75);
+                mHzEdit.setValueComponent(0);
 
-        EditText channelEdit = (EditText) mLpd69View.findViewById(R.id.channel);
-        assertEquals("1", channelEdit.getText().toString());
+                EditText channelEdit = (EditText) mLpd69View.findViewById(R.id.channel);
+                assertEquals("1", channelEdit.getText().toString());
+            }
+        });
     }
 }
