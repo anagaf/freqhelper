0c4b0e1551489c90a9c9f05d7108f70f96bfdf3f
anagaf <Ebnuza5093>
2015-02-28 16:46:20 +0300

    Removed unnecessary sleeps

--- FreqHelper/src/androidTest/java/com/anagaf/freqhelper/ui/ChannelsPageTest.java
+++ FreqHelper/src/androidTest/java/com/anagaf/freqhelper/ui/ChannelsPageTest.java
@@ -49,14 +49,10 @@ public class ChannelsPageTest extends ActivityInstrumentationTestCase2<MainActiv
         TouchUtils.tapView(this, mHzEdit);
         sendKeys("0 ENTER");
 
-        Thread.sleep(1000);
-
         checkChannels(new String[] {"1", "1", INVALID_CHANNEL, INVALID_CHANNEL});
 
         TouchUtils.tapView(this, mMhzEdit);
         sendKeys("4 3 4 ENTER");
-
-        Thread.sleep(1000);
 
         checkChannels(new String[] {"41", INVALID_CHANNEL, INVALID_CHANNEL, INVALID_CHANNEL});
 
@@ -65,14 +61,10 @@ public class ChannelsPageTest extends ActivityInstrumentationTestCase2<MainActiv
         TouchUtils.tapView(this, mHzEdit);
         sendKeys("0 ENTER");
 
-        Thread.sleep(1000);
-
         checkChannels(new String[]{"42", INVALID_CHANNEL, INVALID_CHANNEL, INVALID_CHANNEL});
 
         TouchUtils.tapView(this, mHzEdit);
         sendKeys("5 0 ENTER");
-
-        Thread.sleep(1000);
 
         checkChannels(new String[]{INVALID_CHANNEL, INVALID_CHANNEL, INVALID_CHANNEL, INVALID_CHANNEL});
     }
